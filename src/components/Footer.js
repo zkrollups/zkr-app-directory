1a103830098e77e4aa27bff377f2e5ab360ec315
Joel Kite <joel@numio.one>
2021-12-02 18:05:08 +0000

    Update Footer.js
    fixed link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const PageFooter = ({ align, center, justify, name, pad, path, children }) => (
       <Button icon={<Github color="black" />} hoverIndicator href="https://github.com/zkrollups" target="_blank"/>
     </div>
     <div style={{marginRight:"10px"}}>
-      <a href="https://github.com/ethereum/ethereum-org-website/issues/new/choose" target="_blank" style={{color:"black", fontSize:"medium", marginRight:"10px"}}>Add Project</a>
+      <a href="https://github.com/zkrollups/zkr-app-directory" target="_blank" style={{color:"black", fontSize:"medium", marginRight:"10px"}}>Add Project</a>
       <a href="https://twitter.com/joelkite" target="_blank" style={{color:"black", fontSize:"medium"}}>Get in Touch</a>
     </div>
   </Footer>
