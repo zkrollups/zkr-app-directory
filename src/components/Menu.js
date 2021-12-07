304a8ff0341fcc427a136789984a3dfa086129a1
Joel Kite <joel@numio.one>
2021-12-07 12:54:17 +0000

    Update Menu.js
    split mobile & web wallets

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -34,9 +34,12 @@ const Menu = ({ align, center, justify, name, pad, path, children }) => {
     </Link>
     <Link to="/rollups" style={{ textDecoration: 'none' }}>
       <Button label="Rollups" {...customButtonProps("/rollups")} /> 
+      </Link>
+    <Link to="/mobile" style={{ textDecoration: 'none' }}>
+      <Button label="Mobile" {...customButtonProps("/mobile")} /> 
     </Link>
     <Link to="/wallets" style={{ textDecoration: 'none' }}>
-      <Button label="Wallets" {...customButtonProps("/wallets")} /> 
+      <Button label="Web Wallets" {...customButtonProps("/wallets")} /> 
     </Link>
     <Link to="/infra" style={{ textDecoration: 'none' }}>
       <Button label="Infra" {...customButtonProps("/infra")} /> 
