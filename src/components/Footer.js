698cd7da033ea4d39983d83f0c5dbf1e3968c507
Joel Kite <joel@numio.one>
2021-12-08 13:55:45 +0000

    Update Footer.js
    Footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import {
     Text,
     Footer,
 } from 'grommet';
-import { Github } from 'grommet-icons';
+import { Github, Twitter, Reddit } from 'grommet-icons';
 
 const PageFooter = ({ align, center, justify, name, pad, path, children }) => (
   <Footer background="white" pad="xsmall" border={{ color: '#d3d3d3', size: 'xsmall' }}>
@@ -15,12 +15,13 @@ const PageFooter = ({ align, center, justify, name, pad, path, children }) => (
         &nbsp;&#38;&nbsp; 
         <a href='https://twitter.com/jaazinn' target="_blank" rel="noopener noreferrer" style={{ color: 'black' }}>Jason</a>
     </Text>
-    <div >
-      <Button icon={<Github color="black" />} hoverIndicator href="https://github.com/jaazinn/zkr-app-directory" target="_blank"/>
+    <div>
+    <Button icon={<Github color="black" />} hoverIndicator href="https://github.com/jaazinn/zkr-app-directory" target="_blank"/>
+      <Button icon={<Reddit color="#FF4500" />} hoverIndicator href="https://www.reddit.com/r/zkRollups/" target="_blank"/>
+      <Button icon={<Twitter color="#1DA1F2" />} hoverIndicator href="https://twitter.com/joelkite" target="_blank"/>
     </div>
     <div style={{marginRight:"10px"}}>
-      <a href="https://github.com/jaazinn/zkr-app-directory" target="_blank" rel="noopener noreferrer" style={{color:"black", fontSize:"medium", marginRight:"10px"}}>Add Project</a>
-      <a href="https://twitter.com/joelkite" target="_blank" rel="noopener noreferrer" style={{color:"black", fontSize:"medium"}}>Get in Touch</a>
+      <a href="https://github.com/jaazinn/zkr-app-directory" target="_blank" rel="noopener noreferrer" style={{color:"blue", fontSize:"medium", marginRight:"10px"}}>Add Project</a>
     </div>
   </Footer>
 );
