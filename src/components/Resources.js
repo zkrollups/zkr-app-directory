422d6c017956a375585c9e9e021b311f4262a8f9
Jason Garland <jason9797@gmail.com>
2021-12-08 14:53:42 -0500

    Configure app bar as component

---
+++ src/components/Resources.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Heading,
+} from 'grommet';
+
+class Resources extends Component {
+  render() {
+    return (
+      <div>
+        <Heading level="3">
+          Resources
+        </Heading>
+        
+      </div>
+    )
+  }
+}
+export default Resources;
