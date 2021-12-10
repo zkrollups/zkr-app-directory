4e637c934497ce0fcaf7818f67b3b29443ea8be5
Joel Kite <joel@numio.one>
2021-12-10 14:01:00 +0000

    Update Footer.js
    cleaned up

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,12 +9,6 @@ import { Github, Twitter, Reddit } from 'grommet-icons';
 
 const PageFooter = ({ align, center, justify, name, pad, path, children }) => (
   <Footer background="white" pad="xsmall" border={{ color: '#d3d3d3', size: 'xsmall' }}>
-    <Text pad={{left:"medium"}} size="medium" style={{zoom:"90%", marginLeft:"15px"}}>
-      Made w/&hearts; by&nbsp; 
-        <a href='https://twitter.com/joelkite' target="_blank" rel="noopener noreferrer" style={{ color: 'black' }}>Joel</a>
-        &nbsp;&#38;&nbsp; 
-        <a href='https://twitter.com/jaazinn' target="_blank" rel="noopener noreferrer" style={{ color: 'black' }}>Jason</a>
-    </Text>
     <div>
     <Button icon={<Github color="black" />} hoverIndicator href="https://github.com/jaazinn/zkr-app-directory" target="_blank"/>
       <Button icon={<Reddit color="#FF4500" />} hoverIndicator href="https://www.reddit.com/r/zkRollups/" target="_blank"/>
