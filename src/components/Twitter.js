ba0149c1b0cf00e5be4941f31923a49763e40420
Jason Garland <jason9797@gmail.com>
2022-01-05 13:48:37 -0500

    small styling changes

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -8,14 +8,14 @@ import { TwitterTimelineEmbed } from 'react-twitter-embed';
 class Resources extends Component {
   render() {
     return (
-      <Box direction='row'flex overflow={{ horizontal: 'hidden' }} align='top' justify='center' wrap="true" margin={{left:"medium", right:"medium"}}  style={{zoom:"0.92"}}>
-      <div style={{minWidth:"25%"}}>
+      <Box background='white' direction='row'flex overflow={{ horizontal: 'hidden' }} align='top' justify='center' wrap="true" margin={{left:"none", right:"none"}}  style={{zoom:"0.92"}}>
+      <div style={{minWidth:"25%", paddingTop:"30px"}}>
         <TwitterTimelineEmbed
-  sourceType="list"
-    ownerScreenName="joelkite"
-    slug="1470758011866128394"
-  options={{height: 1000}} 
-/>
+          sourceType="list"
+            ownerScreenName="joelkite"
+            slug="1470758011866128394"
+          options={{height: 1000}} 
+        />
       </div>
       </Box>
     )
