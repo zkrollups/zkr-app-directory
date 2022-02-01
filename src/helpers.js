53a96f0a0b6359613cc024a5936879ebb032166f
Joel Kite <joel@numio.one>
2022-02-01 14:06:50 +0000

    add aztec

--- src/helpers.js
+++ src/helpers.js
@@ -3,5 +3,6 @@ export const NETWORKS = {
     starknet: "https://pbs.twimg.com/media/FE5C0MNXIAQ5xwr.png",
     starkex: "https://starkware.co/wp-content/uploads/2021/06/Group-15-Copy-4.png",
     polygon: "https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png",
-    loopring: "https://cryptologos.cc/logos/loopring-lrc-logo.png"
+    loopring: "https://cryptologos.cc/logos/loopring-lrc-logo.png",
+    aztec: "https://avatars.githubusercontent.com/u/45291733"
 }
