b73ae9ac082500430c4eb7d4aa55f7b95289cc8a
Jason Garland <jason9797@gmail.com>
2022-02-06 19:22:04 -0500

    Fix Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
 Box,
 Grommet,
+Tab,
 Table,
 TableBody,
 TableCell,
@@ -12,59 +13,57 @@ Text,
 } from 'grommet';
 
 // import { grommet } from 'grommet/themes';
-// import { data, columns } from './data';
+import { projects } from '../data/data';
 
 class TokenTable extends Component {
+  allProjects = projects.rollups.concat(projects.featured,projects.dApps,projects.daos,projects.infra,projects.misc,projects.nfts,projects.wallets);
+
   render() {
     return (
-
-
-
-<Box align="center" pad="large">
-<Table caption="Default Table">
-  <TableHeader>
-    <TableRow>
-     <TableCell scope="col" border="bottom">
-        <strong>Name</strong>
-      </TableCell>
-      <TableCell scope="col" border="bottom">
-        <strong>Token Status</strong>
-      </TableCell>
-      <TableCell scope="col" border="bottom">
-        <strong>Ticker</strong>
-      </TableCell>
-      <TableCell scope="col" border="bottom">
-       <strong>Link</strong>
-      </TableCell>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell scope="row" border="bottom">
-        One
-      </TableCell>
-      <TableCell scope="row" border="bottom">Two</TableCell>
-      <TableCell scope="row" border="bottom">Three</TableCell>
-      <TableCell scope="row" border="bottom">  <a href="https://zksync.io/faq/tokenomics.html" rel="noreferrer"> zkSync </a></TableCell>
-    </TableRow>
-     <TableRow>
-      <TableCell scope="row" border="bottom">
-        One
-      </TableCell >
-      <TableCell scope="row" border="bottom">Two</TableCell> 
-      <TableCell scope="row" border="bottom">Three</TableCell>
-<TableCell scope="row" border="bottom">  <a href="https://zksync.io/faq/tokenomics.html" rel="noreferrer"> zkSync </a></TableCell>    </TableRow>
-     <TableRow>
-      <TableCell scope="row" border="bottom">
-        One
-      </TableCell>
-      <TableCell scope="row" border="bottom">Two</TableCell>
-      <TableCell scope="row" border="bottom">Three</TableCell>
-<TableCell scope="row" border="bottom">  <a href="https://zksync.io/faq/tokenomics.html" rel="noreferrer"> zkSync </a></TableCell>    </TableRow>
-  </TableBody>
-</Table>
-</Box>
-
-)}}
+      <Box align="center" pad="large" overflow="auto">
+        <Table caption="zkRollup Projects with Tokens" size="small">
+          <TableHeader>
+            <TableRow>
+            <TableCell scope="col" border="bottom">
+                <strong>Name</strong>
+              </TableCell>
+              <TableCell scope="col" border="bottom">
+                <strong>Token Status</strong>
+              </TableCell>
+              <TableCell scope="col" border="bottom">
+                <strong>Ticker</strong>
+              </TableCell>
+              <TableCell scope="col" border="bottom">
+              <strong>Link</strong>
+              </TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {this.allProjects.map((projects) => {
+              return (
+                <React.Fragment>
+                  {projects.tokenStatus ?
+                    <TableRow key={projects.name} name={projects.category}>
+                      <TableCell scope="row" border="bottom">
+                        {projects.name}
+                      </TableCell>
+                      <TableCell scope="row" border="bottom">
+                        {projects.tokenStatus}
+                      </TableCell>
+                      <TableCell scope="row" border="bottom">
+                        {projects.tokenTicker}
+                      </TableCell>
+                      <TableCell scope="row" border="bottom">  <a href={projects.tokenLink} rel="noreferrer"> {projects.name} </a></TableCell>
+                    </TableRow>
+                  : null}
+                </React.Fragment>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </Box>
+    )
+  };
+};
 
 export default TokenTable;
