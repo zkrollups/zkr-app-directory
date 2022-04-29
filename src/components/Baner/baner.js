ffe74698103d81ca929a76a89f22747bc95e0d3c
Umair S <umair@divsync.com>
2022-04-29 16:30:18 +0500

    adding search/filtering fixes for show more

---
+++ src/components/Baner/baner.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./style.css"
+import SearchBar from '../Searchbar/SearchBar'
+function Baner() {
+  return (
+   <>
+   
+   <div className='container-fluied'>
+       
+       <div className='bner_wrap'>
+
+<h1 className='text-center text_heading'>Discover the <span className='span_text'>future </span>of <span className='span_text'>Ethereum </span>on Layer 2</h1>
+
+
+<SearchBar/>
+       </div>
+
+   </div>
+   
+   </>
+  )
+}
+
+export default Baner
