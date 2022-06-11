4be17ff58aafd48ccb2f8b2fb16f9db34bd9f6e1
Jason Garland <jason9797@gmail.com>
2022-06-11 00:27:57 -0400

    Configure mailing list signup

--- src/components/stay_updated/index.js
+++ src/components/stay_updated/index.js
@@ -5,58 +5,40 @@ import stay_img from '../../actets/stay.png';
 function stay() {
   return (
    <>
-   
 
-<div className=' main_c '>
-<div className='row main_wrap'>
-	<div className=' col-sm-12 col-lg-6 col-md-12 img_part mb-3'>
-<div className='col-2'>
-<img src={stay_img} alt="" width={'65px'} height={"73px"}></img>
-</div>
-<div className='col-10 spacing_left'>
-	<div>
-	<span class="stay">Stay Updated</span>
-	</div>
-<div className='my-2 subtitle_be'>
-<span >Be the first to know about Ethereum Layer 2 news</span>
-</div>
+	<div className=' main_c '>
+		<div className='row main_wrap'>
+			<div className=' col-sm-12 col-lg-6 col-md-12 img_part mb-3'>
+				<div className='col-2'>
+					<img src={stay_img} alt="" width={'65px'} height={"73px"}></img>
+				</div>
+				<div className='col-10 spacing_left'>
+					<div>
+						<span class="stay">Stay Updated</span>
+					</div>
+					<div className='my-2 subtitle_be'>
+						<span >Be the first to know about Ethereum Layer 2 news</span>
+					</div>
+				</div>
+			</div>
 
+			<div className='col-sm-12 col-lg-6 col-md-12 spacing_2'>
+				{/* <div className='src_side p-2'> */}
+				<form action="https://xyz.us18.list-manage.com/subscribe/post" method="POST" className='src_side p-2'>
+					<input type="hidden" name="u" value="fce65f98b56135d3e8b86ca2a"></input>
+    				<input type="hidden" name="id" value="0789ac1979"></input>
+
+					<div>
+						<input className='inpp' placeholder='Enter Your Email ' name="MERGE0" id="MERGE0"></input>
+					</div>
+					<div>
+						<button className='subsub'> Subscribe</button>
+					</div>
+				</form>
+			</div>
 			
-</div>
-
-	</div>
-	<div className='col-sm-12 col-lg-6 col-md-12 spacing_2'>
-		<div className='src_side p-2'>
-			<div>	<input className='inpp' placeholder='Enter Your Email '></input></div>
-	<div>
-		<button className='subsub'> Subscribe</button>
-	</div>
 		</div>
 	</div>
-
-</div>
-
-</div>
-
-
-
-   {/* <div class="v41_807">
-			<div class="v41_802"></div>
-		
-			<div class="v41_832">
-                <img src={stay_img}></img>
-			
-			</div>
-			<div class="v41_835 ">
-			
-				<div class="v41_804">
-					<span class="v41_805">Subscribe</span>
-				</div>
-				<span  class="v41_806">Enter your email address</span>
-			</div>
-			<span class="v41_827">Be the first to know about Ethereum Layer 2 news</span>
-			<span class="v41_870">Stay Updated</span>
-		</div> */}
    
    </>
   )
